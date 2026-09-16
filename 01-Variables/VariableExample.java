ccd82b40b7452f01a033770516fddc2f78260582
Shruti Shelake <shrutishelake31@gmail.com>
2026-09-16 21:24:58 +0530

    Create VariableExample.java

---
+++ 01-Variables/VariableExample.java
@@ -0,0 +1,12 @@
+class VariableExample {
+    public static void main(String[] args) {
+
+        String name = "Shruti";
+        int age = 20;
+        String food = "Pizza";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(food);
+    }
+}
